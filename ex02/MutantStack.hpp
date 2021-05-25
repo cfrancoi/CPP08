5048deb556c17c030b530443e3eb4ca47312119c
cfrancoi <cfrancoi@z4r8p7.42lyon.fr>
2021-05-25 18:54:25 +0200

    mutant stack patch

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,133 +1,14 @@
 
-template<typename Type>
-struct  Node
-{
-	Type		el;
-	Node<Type> *		next;
-	Node<Type> &		operator++() { return *(this->next); }
-	
 
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type type;
+		typedef typename type::iterator iterator;
+
+		iterator	begin() { return std::stack<int>::c.begin(); }
+		iterator	end() { return std::stack<int>::c.end(); }
 };
-
-template<typename Type>
-std::ostream &			operator<<( std::ostream & o, Node<Type> const & i )
-{
-	o << i.el;
-	return o;
-}
-
-template<typename Type>
-class MutantStack
-{
-public:
-	MutantStack();
-	~MutantStack();
-
-	typedef Node<Type>* iterator;
-	typedef Node<Type>	node_t;
-
-	void			push(Type to_push);
-
-
-	size_t			size();
-
-	node_t			&top();
-	
-	iterator		begin();
-	iterator		end();
-	private:
-		iterator	_tab;
-		iterator	it;
-};
-
-template<typename Type>
-MutantStack<Type>::MutantStack() 
-{
-	_tab = 0;
-	it = 0;
-}
-
-template<typename Type>
-MutantStack<Type>::~MutantStack() 
-{
-	iterator start;
-	iterator tmp;
-
-	start = begin();
-
-	while (start != NULL)
-	{
-		tmp = start->next;
-		delete start;
-		start = tmp;
-	}
-}
-
-template<typename Type>
-void MutantStack<Type>::push(Type to_push) 
-{
-	iterator it;
-
-	it = _tab;
-	if (_tab == NULL)
-	{
-		_tab = new node_t();
-		_tab->el = to_push;
-		_tab->next = 0;
-		return;
-	}
-	while((it->next) != 0)
-	{
-		++it;
-	}
-	it->next = new node_t();
-	++it;
-	it->el = to_push;
-	it->next = NULL;
-}
-
-template<typename Type>
-size_t MutantStack<Type>::size() 
-{
-	iterator	it;
-	size_t		ret;
-
-	ret = 0;
-	it = _tab;
-
-	if (_tab == 0)
-		return (0);
-	if (_tab)
-		ret++;
-	while (it->next != NULL)
-	{
-		++it;
-		++ret;
-	}
-	return ret;
-	
-}
-
-template<typename Type>
-typename MutantStack<Type>::node_t&		MutantStack<Type>::top() 
-{
-	return(*_tab);
-}
-
-template<typename Type>
-typename MutantStack<Type>::iterator MutantStack<Type>::begin() 
-{
-	return (_tab);
-}
-
-template<typename Type>
-typename MutantStack<Type>::iterator MutantStack<Type>::end() 
-{
-	iterator it;
-
-	it = _tab;
-
-	while(it->next)
-		++it;
-	return it;
-}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,32 +4,32 @@
 #include "MutantStack.hpp"
 
 #include <stack>
-
+#include <deque>
 #include <list>
 
 int main(void)
 {
-	MutantStack<int> itm;
-
-	std::cout << itm.top() << "|" << itm.top() << std::endl;
-	itm.push(15);
-	itm.push(10);
-	itm.push(10);
-	itm.push(10);
-	itm.push(10);
-
-	MutantStack<int>::iterator it;
-
-	it = itm.begin();
-
-	std::cout << itm.top() << "|" << itm.size() << std::endl;
-
-	while (it != itm.end())
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-
+	std::stack<int> s(mstack);
 	return 0;
 }
 
