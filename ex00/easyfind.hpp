5cefc3bb0fc0cf96fdd4ff62384d4d59c4d354cd
Corentin Francois <cfrancoi@z4r8p7.42lyon.fr>
2021-06-07 18:44:22 +0200

    patch

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,11 +1,10 @@
+#ifndef __EASYFIND_H__
+#define __EASYFIND_H__
+#include <algorithm>
 
 template< typename T>
 typename T::iterator	easyfind(T & el, int to_find)
 {
-	typename T::iterator	it;
-
-	for(it = el.begin(); it != el.end(); it++)
-		if (*it == to_find)
-			break;
-	return it;
+	return (find(el.begin(), el.end(), to_find));
 }
+#endif // __EASYFIND_H__
